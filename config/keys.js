23b1cd3e202b7e56015231fa75476053f487bc11
Alessandro Staffolani <alessandro.staffolani.93@gmail.com>
2018-09-15 00:50:20 +0200

    Finished server's api

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://udemy:udemy1@ds145562.mlab.com:45562/devconnector',
-    secretOrKey: 'secret'
+  mongoURI: 'mongodb://udemy:udemy1@ds145562.mlab.com:45562/devconnector',
+  secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
@@ -18,17 +18,17 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
-// Passport Config 
+// Passport Config
 require('./config/passport')(passport);
 
-// Use Routes 
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
