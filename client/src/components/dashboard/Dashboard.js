b5fc7db64cfbb9001f0eaa05d81b41b0df622f41
Alessandro Staffolani <alestam93@gmail.com>
2018-11-08 00:02:12 +0100

    Added experience and education sections (Add and Delete)

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
+import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
     componentDidMount() {
@@ -12,7 +14,7 @@ class Dashboard extends Component {
     }
 
     onDeleteClick(e) {
-      this.props.deleteAccount();
+        this.props.deleteAccount();
     }
 
     render() {
@@ -31,7 +33,8 @@ class Dashboard extends Component {
                             Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
                         </p>
                         <ProfileActions />
-                        {/* TODO: exp and edu */}
+                        <Experience experience={profile.experience} />
+                        <Education education={profile.education} />
                         <div style={{ marginBottom: '60px' }}>
                             <button className="btn btn-danger" onClick={this.onDeleteClick.bind(this)}>
                                 Delete My Account
